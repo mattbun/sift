0b7e12f1ec667baa7b3b4eaab6a0a7ae856ede3f
Matt Rathbun <matt@rathbook.local>
2014-05-20 11:24:19 -0800

    filepaths can be used as arguments without having to use '--file', '--audio', or '--video'

--- sift.py
+++ sift.py
@@ -12,6 +12,7 @@ def recurse(path, conf, assemblePath):
     
     if os.path.isfile(path):
         fileutils.doSomethingWithFile(path, assemblePath(conf, path), conf['file_operation'])
+
     else:
         for root, subFolders, files in os.walk(path):
 
@@ -26,6 +27,7 @@ def recurse(path, conf, assemblePath):
 
 # Parse Arguments
 parser = argparse.ArgumentParser(description='Move files based on perceived or existing metadata.')
+parser.add_argument('paths', metavar='PATH', nargs='*', help='Sort a file/folder and let sift determine what type of file it is.')
 parser.add_argument('--file', '-f', metavar='PATH', nargs=1, action='append', help='Sort a file/folder and let sift determine what type of file it is.')
 parser.add_argument('--video', '-v', metavar='PATH', nargs=1, action='append', help='Sort file/folder as video')
 parser.add_argument('--audio', '-a', metavar='PATH', nargs=1, help='Sort file/folder as audio')
@@ -34,27 +36,30 @@ args = parser.parse_args()
 # Parse Configuration file
 conf = config.config
 
+if args.paths is not None:
+    from lib import file
+    for f in args.paths:
+        recurse(f, conf, file.process)
+
 if args.file is not None:
     from lib import file
     for f in args.file:
         recurse(f[0], conf, file.process)
-        #print(file.process(conf, f[0]))
 
 if args.audio is not None:
     from lib import audio
     for a in args.audio:
         recurse(a, conf, lib.audio.assemblePath)
-        #print(lib.audio.process(conf, a))
 
 if args.video is not None:
     from lib import video
     for v in args.video:
         recurse(v[0], conf, video.process)
-        #print(video.process(conf, v[0]))
 
 if args.video is None and \
    args.audio is None and \
-   args.file is None:
+   args.file is None and \
+   args.paths is None:
     from lib import watch
     watch.start(conf) 
 
