daf402302e1079728c92b73bb77816a7e36c0082
Matt Rathbun <matt@rathbook.rathbun.cx>
2014-07-26 21:20:01 -0400

    Use python's mimetypes to determine file type

--- lib/file.py
+++ lib/file.py
@@ -2,15 +2,17 @@ import lib
 from lib import utils
 from lib import audio
 from lib import video
+import mimetypes
 
-extension_map = {
-    "mp3" : audio.assemblePath,
-    "avi" : video.process,
-    "mkv" : video.process,
-    "mp4" : video.process
+mime_map = {
+    "audio" : audio.assemblePath,
+    "video" : video.process
 }
 
 def process(conf, full_path):
-    extension = utils.getExtension(full_path)
-    return extension_map[extension](conf, full_path)
+    
+    mimetype = mimetypes.guess_type(full_path)
+    type = mimetype[0].partition('/')[0]
+
+    return mime_map[type](conf, full_path)
 
