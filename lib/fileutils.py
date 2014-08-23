e87027f88bfade770178806c02fa7d18f1a86f8f
Matt Rathbun <matt@rathbook.local>
2014-08-22 21:29:18 -0400

    Small fixes to file operations

--- lib/fileutils.py
+++ lib/fileutils.py
@@ -9,25 +9,30 @@ def mkdirsInPath(path):
     except OSError as exc: 
         if exc.errno == errno.EEXIST and os.path.isdir(dirpath):
             pass
+        else:
+            return 0
+
+    return 1
 
 def dryrun(source, destination):
     print(source + "\n\t-> " + destination)
 
 def copy(source, destination):
     print(source)
-    mkdirsInPath(destination) #TODO rollback
-    from shutil import copy
-    shutil.copy(source, destination)
-    print("\t-copied-to-> " + destination)
+    if (mkdirsInPath(destination)): #TODO rollback
+        from shutil import copy
+        shutil.copy(source, destination)
+        print("\t-copied-to-> " + destination)
 
 def move(source, destination):
     print(source)
-    mkdirsInPath(destination) #TODO rollback
-    from shutil import move
-    shutil.move(source, destination)
-    print("\t-moved-to-> " + destination)
+    if (mkdirsInPath(destination)): #TODO rollback
+        from shutil import move
+        shutil.move(source, destination)
+        print("\t-moved-to-> " + destination)
 
-extension_map = {
+
+operation_map = {
     "move" : move,
     "copy" : copy,
     "dryrun" : dryrun,
@@ -35,6 +40,6 @@ extension_map = {
 }
 
 def doSomethingWithFile(source, destination, operation):
-    return extension_map[operation](source, destination)
+    return operation_map[operation](source, destination)
 
 
